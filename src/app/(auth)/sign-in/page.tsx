f16677f877530cc08a4624b886c13dc999bdd957
danishRajawat <ibnfarooq070@gmail.com>
2026-01-10 14:14:28 +0530

    sign up api integreated

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,19 +1,11 @@
-"use client";
-import React, { useState } from "react";
-import {useDebounceValue} from "usehooks-ts"
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import Link from "next/link";
+import React from 'react'
 
-const Page = () => {
-  const [username, setUsername] = useState("");
-  const [usernameMessage, setUsernameMessage] = useState("");
-  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
-  const [isSubmiting, setIsSubmiting] = useState(false);
-const debouncedUsername =  useDebounceValue(username , 300)
+function page() {
+  return (
+    <div>
+      sign in
+    </div>
+  )
+}
 
-  return <div></div>;
-};
-
-export default Page;
+export default page
